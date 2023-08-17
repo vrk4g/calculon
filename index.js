65d4e6d20e7b4224244dc11176d09c9e4f9341b0
vrk4g <44714433+vrk4g@users.noreply.github.com>
2023-08-17 17:48:52 +0500

    Add a function calling the basic math operations functions

--- index.js
+++ index.js
@@ -14,6 +14,23 @@ function divide(a, b) {
     return a / b;
 }
 
+function operate(operator, firstNumber, secondNumber) {
+    switch (operator) {
+        case "add":
+            add(firstNumber, secondNumber);
+            break;
+        case "subtract":
+            subtract(firstNumber, secondNumber);
+            break;
+        case "multiply":
+            multiply(firstNumber, secondNumber);
+            break;
+        case "divide":
+            divide(firstNumber, secondNumber);
+            break;
+    }
+}
+
 let firstNumber = 3;
 let operator = "add";
 let secondNumber = 5;
